e4e779dee39a60fa18152aef666ac32481ee219d
Kiran <u1143683@utah.edu>
2017-09-15 15:28:19 -0600

    "Update"

--- ngrams/ngram_extract.py
+++ ngrams/ngram_extract.py
@@ -1,14 +1,24 @@
 import string
 from collections import Counter
-def extract_unigrams(train_file):
+def extract_unigrams(train_file, phi = 0):
     unigrams = []
     with open(train_file) as f:
         for x in f:
-            unigrams.extend(x.strip().split(" "))
-    unigrams = [y.lower() for y in unigrams]
-    #unigrams = [x for x in unigrams if len(x) > 0 and x not in set(string.punctuation)]
-    unigrams = [x for x in unigrams if len(x) > 0]
-    return Counter(unigrams)
+            a = ["phi"]
+            if(phi == 0):
+                a = []
+            a.extend(x.strip().split(" "))
+            a = [y.lower() for y in a]
+            a = [x for x in a if len(x) > 0]
+            unigrams.append((x,a))
+    return unigrams
+
+def extract_unigram_models(train_file, phi = 0):
+    l = []
+    for x in extract_unigrams(train_file, phi):
+        l.extend(x[1])
+    return Counter(l)
+
 
 def extract_bigrams(train_file):
     bigrams = []
@@ -21,5 +31,11 @@ def extract_bigrams(train_file):
             bigram_c = bigram[1:]
             bigram = bigram[:-1]
             bigram = list(zip(bigram, bigram_c))
-            bigrams.extend(bigram)
-    return Counter(bigrams)
+            bigrams.append((x,bigram))
+    return bigrams
+
+def extract_bigram_models(train_file):
+    l = []
+    for x in extract_bigrams(train_file):
+        l.extend(x[1])
+    return Counter(l)
